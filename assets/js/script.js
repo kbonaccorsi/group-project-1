61fa066ea6c4f8254fd54f06068059d4d4b8bb56
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-14 12:37:55 -0500

    food section is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,17 +43,58 @@ function recipeApi(region) {
     console.log(region) // want to append this region onto the fetch request based on api specs
 
     fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${pickRandom(foodQueries)}&app_id=1802ebf5&app_key=c4815f7f4cc77e6493a25d7413cc910b&cuisineType=${region}`)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            console.log(pickRandom(response.hits))
+        .then(function (response) {
+            return response.json()
         })
+        .then(function (data) {
+            console.log(data)
+
+            let result = pickRandom(data.hits);
+            console.log(result);
+
+            let foodContainer = document.querySelector("#food-container");
+            let div = document.createElement("div");
+            // food image
+            let img = document.createElement("img");
+            //food title
+            let foodTitle = document.createElement("h3");
+            // link to recipe
+            let url = document.createElement("a");
+            
+            // let healthLabels = document.createElement("ul");
+
+
+
+
+            foodTitle.textContent = result.recipe.label;
+            console.log(foodTitle)
+            foodContainer.appendChild(foodTitle);
+
+            img.setAttribute("src", result.recipe.image);
+            console.log(img);
+            foodContainer.appendChild(div);
+            div.classList.add("food-results");
+            div.appendChild(img);
+
+            url.setAttribute("href", result.recipe.url);
+            url.setAttribute("target", "_blank");
+            url.textContent = "recipe HERE";
+            console.log(url);
+            div.appendChild(url);
+
+            // healthLabels = ""
+            // for (let i = 0; i < result.recipe.healthLabels.length; i++) {
+            // healthLabels.textContent = result.recipe.healthLabels[i];
+            // console.log(healthLabels);
+            // div.appendChild(healthLabels);
+            // }
+        });
 };
 
 function movieApi(region) {
     console.log(region);
     //append the region string onto the fetch request based on api specs
-    fetch("https://imdb-api.com/en/API/Top250Movies/k_50wgorsx")
+    fetch("_blankhttps://imdb-api.com/en/API/Top250Movies/k_50wgorsx")
         .then(response => response.json())
         .then(response => {
             console.log(pickRandom(response.items))
@@ -118,53 +159,3 @@ function getMovieResults(movieResults) {
     }
 };
 
-
-
-// FOOD CONTAINER
-
-function getFoodResults(foodResults) {
-    for (let i = 0; i < 5; i++) {
-        console.log(foodResults[i]);
-        let foodContainer = document.querySelector("#food-container");
-        let div = document.createElement("div");
-
-        // food image
-        let img = document.createElement("img");
-
-        //food title
-        let foodTitle = document.createElement("h4");
-
-        // list of recipe information
-        let information = document.createElement("ul");
-
-        //cooktime
-        let cookTime = document.createElement("li");
-
-        //description
-        let description = document.createElement("li");
-
-        //special notes available
-        let specialNotes = document.createElement("li");
-
-
-
-
-        foodTitle.textContent = "Title: " + insertAPI.title;
-        img.setAttribute("src", "******insert API.link");
-        img.setAttribute("alt", foodTitle + "poster")
-        cookTime.textContent = "cookTime: " + insertAPI.cookTime;
-        description.textContent = "Description: " + insertAPI.description;
-        specialNotes.textContent = "Available specialNotes: " + insertAPI.language;
-
-
-
-        foodContainer.appendChild(div)
-        div.classList.add("food-results")
-        div.appendChild(foodTitle)
-        div.appendChild(img)
-        div.appendChild(information)
-        information.appendChild(cookTime)
-        information.appendChild(description)
-        information.appendChild(specialNotes)
-    }
-};
