d32fdaab5b8c8665b05b40e32007011f1dbb9c26
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-15 09:20:22 -0500

    pushing most updated version with fetch calls in one function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,92 @@
 // Global Variables 
-let container = document.querySelector(".nav")
-let results = document.querySelector(".section");
+
+//navigation bar to make the buttons have some action
+let container = document.querySelector(".nav");
+//recipe name
 let foodTitle = document.querySelector("#food-title");
+//image of recipe
 let foodImg = document.querySelector("#food-img");
+//url to view recipe
 let foodUrl = document.querySelector("#food-link");
+// makes the opening phrase disappear when results display
 let recipeAction = document.querySelector("#recipe-action");
+//recipe h2
 let recipeSuggestion = document.querySelector("#recipe-suggestion");
+//h3 movie title display
 let movieTitle = document.querySelector("#movie-title");
-let btnResult = [];
+//image of movie poster
 let movieImg = document.querySelector("#movie-img");
+// makes the opening phrase disappear when results display
 let movieAction = document.querySelector("#movie-action");
+//movie h2
 let titleSuggestion = document.querySelector("#movie-suggestion");
 
 
-// get data attribute from the buttons. 
-// then do fetch calls 
-// grab appropriate parameters from fetch. 
-
+//assigns the API parameter to choose from one of these choices
 const foodQueries = ["beef", "chicken", "fish", "pork", "vegetarian"]
 
-function pickRandom(arr) { // gets a random element from an array and returns it
+// gets a random element from an array and returns it
+function pickRandom(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-function displayResults(region) {
-    recipeApi(region); // foodApi('american')
-    movieApi(region); // recipeApi('american')
-};
-
-function recipeApi(region) {
-    console.log(region) // want to append this region onto the fetch request based on api specs
+//uses the APIs and parameters to return suggestion names, images, and recipe url
+function apis(region) {
+    console.log(region)
 
     fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${pickRandom(foodQueries)}&app_id=1802ebf5&app_key=c4815f7f4cc77e6493a25d7413cc910b&cuisineType=${region}`)
         .then(response => response.json())
         .then(data => {
-            let resultF = pickRandom(data.hits);
-            foodTitle.textContent = resultF.recipe.label;
-            foodImg.setAttribute("src", resultF.recipe.image);
-            foodUrl.setAttribute("href", resultF.recipe.foodUrl);
+            let resultsF = pickRandom(data.hits);
+            foodTitle.textContent = resultsF.recipe.label;
+            foodImg.setAttribute("src", resultsF.recipe.image);
+            foodUrl.setAttribute("href", resultsF.recipe.url);
             foodUrl.setAttribute("target", "_blank");
             foodUrl.textContent = "Click here to view the recipe";
             recipeAction.setAttribute("class", "hide");
             recipeSuggestion.removeAttribute("class");
+            console.log(resultsF);
+            fetch("https://imdb-api.com/en/API/Top250Movies/k_tq6blagh")
+                .then(response => response.json())
+                .then(data => {
+                    let resultsM = pickRandom(data.items);
+                    movieTitle.textContent = "Title: " + resultsM.title;
+                    movieImg.setAttribute("src", resultsM.image);
+                    movieAction.setAttribute("class", "hide");
+                    titleSuggestion.removeAttribute("class");
+                    console.log(resultsM);
+                })
         });
 };
 
-function movieApi(region) {
-    console.log(region);
-    //append the region string onto the fetch request based on api specs
-    fetch("https://imdb-api.com/en/API/Top250Movies/k_tq6blagh")
-        .then(response => response.json())
-        .then(response => {
-            let resultsM = pickRandom(response.items);
-            movieTitle.textContent = "Title: " + resultsM.title;
-            movieImg.setAttribute("src", resultsM.image);
-            movieAction.setAttribute("class", "hide");
-            titleSuggestion.removeAttribute("class");
-        })
+//puts the food and movie suggestions on the webpage
+function displayResults(region) {
+    apis(region);
 };
 
-function init() {
-    let storedPastParings = JSON.parse(localStorage.getItem("movie"));
-    if (!storedPastParings === null) {
-        btnResult = storedPastParings
-    }
-    movieApi();
-}
 
-function storePastParings() {
-    localStorage.setItem("movie", JSON.stringify(btnResult));
-    // let pastMovie = (movieTitle.value)
-    // console.log(pastMovie);
-    // let displayPastMovie = document.querySelector("#last-movie")
-    // localStorage.getItem(pastMovie);
+function renderPastSearches() {
+
 };
+
+function displayPastSearches() {
+
+};
+
+
+// function init() {
+//     let storedPastParings = JSON.parse(localStorage.getItem("movie"));
+//     if (!storedPastParings === null) {
+//         btnResult = storedPastParings
+//     }
+// }
+
+// function storePastParings() {
+//     localStorage.setItem("movie", JSON.stringify(btnResult));
+//     // let pastMovie = (movieTitle.value)
+//     // let displayPastMovie = document.querySelector("#last-movie")
+//     // localStorage.getItem(pastMovie);
+// };
 
 // utilitiy if we need it---might not--good for conversions
 // function regionHandlerForMovies(region) {
@@ -81,14 +94,13 @@ function storePastParings() {
 //         return 'US'
 //     }
 // };
-console.log(movieTitle);
 
+//adds a click event to the entire navigation bar so any button clicked will have an action
 container.addEventListener("click", function (event) {
     let element = event.target
     if (element.matches(".button")) {
         let state = element.getAttribute("id");
         displayResults(state) //displayResults('american')
     }
-
 });
 
